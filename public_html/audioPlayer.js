ecdbdad54a0ee346953cb477a18ec177048a4586
ohn0 <silverskinx@gmail.com>
2017-10-30 21:37:21 -0400

    Started working on ripple.

--- public_html/audioPlayer.js
+++ public_html/audioPlayer.js
@@ -12,6 +12,9 @@ function audioFW(params){
     aPlayer.cDiv.style.borderStyle = "solid";
     aPlayer.cDiv.style.borderColor = "white";
     aPlayer.requestVar = null;
+    var rippleList = [];
+    var rippleCounter = 0;
+    var rippleLimit = 100;
 
     if(aPlayer.cDiv === null){
         alert("No div specified, quitting.");
@@ -26,7 +29,6 @@ function audioFW(params){
     aPlayer.setBufferSize = function(bufferSize){
         aCTX.analyser.fftSize = bufferSize;
         return null;
-
     };
     
     aPlayer.pulse = function(){
@@ -42,9 +44,10 @@ function audioFW(params){
         aPlayer.audioDIV.appendChild(aPlayer.audioEle);
         aPlayer.audioEle.controls = true;
         aPlayer.audioEle.volume = 0.1;
+        aPlayer.audioEle.id = params.fwID;
         aPlayer.audioEle.onpause = aPlayer.stopVis;
         aPlayer.audioEle.onplay = aPlayer.startVis;
-        aPlayer.audioEle.src = aPlayer.fileList[0];
+        aPlayer.audioEle.src = aPlayer.fileList[1];
         aPlayer.cDiv.appendChild(aPlayer.audioDIV);
         return null;
     }
@@ -76,8 +79,6 @@ function audioFW(params){
         return null;
     }
     
-
-    
     aPlayer.barVisual = function(timestamp){
         if(aPlayer.isPlaying){ 
             aPlayer.animator.ctx.clearRect(0,0,aPlayer.cWidth,aPlayer.cHeight);
@@ -85,9 +86,8 @@ function audioFW(params){
         aPlayer.CTX.analyser.getByteFrequencyData(aPlayer.animator.freqData);
         aPlayer.CTX.analyser.getFloatFrequencyData(aPlayer.animator.floatFreqData);
         for(var i = 0; i < aPlayer.CTX.analyser.fftSize/2; i++){
-            aPlayer.animator.ctx.fillStyle = 'rgb(0,' + 
-                    ((i*aPlayer.animator.rectWidth)/aPlayer.cWidth) * 255 + ','+
-                    '128)';
+            changeColor('rgb(0,'+((i*aPlayer.animator.rectWidth)/aPlayer.cWidth)
+                    *255 + ',128)');
             aPlayer.animator.ctx.fillRect(i*aPlayer.animator.rectWidth,
                     (400)-aPlayer.cHeight * (aPlayer.animator.freqData[i]/255),
                     aPlayer.animator.rectWidth, aPlayer.cHeight * 
@@ -100,19 +100,46 @@ function audioFW(params){
         aPlayer.animator.ctx.clearRect(0,0, aPlayer.cWidth, aPlayer.cHeight);
         aPlayer.CTX.analyser.getByteFrequencyData(aPlayer.animator.freqData);
         aPlayer.CTX.analyser.getFloatFrequencyData(aPlayer.animator.floatFreqData);
+//        LOG(aPlayer.animator.floatFreqData[0]);
         aPlayer.animator.ctx.beginPath();
         aPlayer.animator.ctx.moveTo(0, aPlayer.cHeight/2);
         for(var i = 0; i < aPlayer.CTX.analyser.fftSize/2; i++){
+            changeColor('rgb(0,'+((i*aPlayer.animator.rectWidth)/aPlayer.cWidth)
+                    *255 + ',128)');
             aPlayer.animator.ctx.lineTo(i*aPlayer.animator.rectWidth,
                            (aPlayer.cHeight/2) - ((aPlayer.cHeight/2) *
                                    (aPlayer.animator.freqData[i]/255)));
         }
         aPlayer.animator.ctx.lineTo(aPlayer.cWidth, aPlayer.cHeight/2);
         aPlayer.animator.ctx.lineWidth = 2;
-        aPlayer.animator.ctx.strokeStyle = "red";
+        aPlayer.animator.ctx.strokeStyle = "rgb(0,"+ 
+               (255 * (document.getElementById(aPlayer.audioEle.id).currentTime)/
+                document.getElementById(aPlayer.audioEle.id).duration) +",128)";
         aPlayer.animator.ctx.stroke();
     };
     
+    aPlayer.rippleVisual = function(timestamp){
+        rippleCounter++;
+        rippleList[rippleCounter % rippleLimit] = ripples({
+            x: Math.random() * aPlayer.cWidth,
+            y: Math.random() * aPlayer.cHeight
+        });
+        aPlayer.animator.ctx.strokeStyle = "white";
+        aPlayer.animator.ctx.clearRect(0,0, aPlayer.cWidth, aPlayer.cHeight);
+        for(var i = 0; i < Math.min(rippleCounter, rippleLimit); i++){
+            aPlayer.animator.ctx.beginPath();
+//            LOG(rippleList[i]);
+            aPlayer.animator.ctx.arc(rippleList[i].x, rippleList[i].y,
+                                    rippleList[i].radius, 0, 2*Math.PI);
+            rippleList[i].grow();
+            aPlayer.animator.ctx.stroke();
+        }
+    };
+    
+    function changeColor(colorVal){
+        aPlayer.animator.ctx.fillStyle = colorVal;
+        return null;
+    }
     
     aPlayer.startVis = function(){
         aPlayer.requestVar = window.requestAnimationFrame(aPlayer.visualFn);
@@ -127,6 +154,9 @@ function audioFW(params){
     if(params.visualStyle === "bar"){
         aPlayer.visualFn = aPlayer.barVisual;
     }
+    else if(params.visualStyle === "ripple"){
+        aPlayer.visualFn = aPlayer.rippleVisual;
+    }
     else if(params.visualStyle === "line"){
         aPlayer.visualFn = aPlayer.lineVisual;
     }
@@ -139,11 +169,28 @@ function audioFW(params){
             aPlayer.requestVar = window.requestAnimationFrame(aPlayer.visualFn);
         },25);
     }
+    
     document.body.onresize = function(){
         aPlayer.animator.rectWidth = Math.ceil(aPlayer.cDiv.clientWidth / 
                 (aPlayer.CTX.analyser.fftSize/2));
+        aPlayer.animator.canvasEle.width = aPlayer.cDiv.clientWidth;
         aPlayer.cWidth = aPlayer.cDiv.clientWidth;
     };
-    LOG('done');
+    LOG('Done');
     return aPlayer;
+}
+
+function ripples(params){
+    var rippleList = {};
+    rippleList.x = params.x;
+    rippleList.y = params.y;
+    rippleList.radius = 2;
+    
+    rippleList.grow = function(){
+        if(rippleList.radius < 100){
+            rippleList.radius += 2;
+        }
+    };
+    
+    return rippleList;
 }
