dc15b77a5a0df19a7d736ba3e47f4e46e8e2bf36
ohn0 <silverskinx@gmail.com>
2017-11-01 23:10:07 -0400

    Working on circle.

--- public_html/audioPlayer.js
+++ public_html/audioPlayer.js
@@ -11,45 +11,101 @@ function audioFW(params){
     aPlayer.isPlaying = true;
     aPlayer.cDiv.style.borderStyle = "solid";
     aPlayer.cDiv.style.borderColor = "white";
+    aPlayer.colorFn = params.colorFn || function(params){
+        var color = "rgb("+
+        ((params.time)/params.duration)* 255+
+        ",128,0)";
+         return color;
+    };
+    aPlayer.mainID = params.contextID;
     aPlayer.requestVar = null;
-    var rippleList = [];
-    var rippleCounter = 0;
-    var rippleLimit = 100;
 
     if(aPlayer.cDiv === null){
         alert("No div specified, quitting.");
         return null;
     }
     
-    aPlayer.addMusicFile = function(musicFile){
+    aPlayer.addFile = function(musicFile){
         aPlayer.fileList.push(musicFile);
+        updateSongList();
         return null;
     };
     
-    aPlayer.setBufferSize = function(bufferSize){
+    
+    aPlayer.setQuality = function(bufferSize){
+        //bufferSize MUST be a power of two.
         aCTX.analyser.fftSize = bufferSize;
         return null;
     };
     
-    aPlayer.pulse = function(){
-        return null;
-    };
     
     function createPlayer(){
         aPlayer.audioDIV = document.createElement('div');
+        aPlayer.audioDIV.nextButton = document.createElement('button');
+        aPlayer.audioDIV.prevButton = document.createElement('button');
+        aPlayer.audioDIV.list = document.createElement('div');
+        aPlayer.audioDIV.style.margin = "auto";
+        aPlayer.audioDIV.list.style.color = "white"; 
+        aPlayer.audioDIV.style.textAlign = "auto";
+        aPlayer.audioDIV.nextButton.type = "button";
+        aPlayer.audioDIV.nextButton.value = "next";
+        aPlayer.audioDIV.nextButton.innerHTML = "next";
+        aPlayer.audioDIV.prevButton.type = "button";
+        aPlayer.audioDIV.prevButton.value = "previous";
+        aPlayer.audioDIV.prevButton.innerHTML = "previous";
         aPlayer.audioEle = document.createElement('audio');
-        aPlayer.audioDIV.style.minWidth = "80%";
         aPlayer.audioDIV.style.margin = "auto";
         aPlayer.audioDIV.style.textAlign = "center";
+        aPlayer.audioDIV.appendChild(aPlayer.audioDIV.prevButton);
         aPlayer.audioDIV.appendChild(aPlayer.audioEle);
+        aPlayer.audioDIV.appendChild(aPlayer.audioDIV.nextButton);
+        aPlayer.audioDIV.appendChild(aPlayer.audioDIV.list);
+        setButtonFn();
         aPlayer.audioEle.controls = true;
-//        aPlayer.audioEle.volume = 0.1;
         aPlayer.audioEle.id = params.fwID;
         aPlayer.audioEle.onpause = aPlayer.stopVis;
         aPlayer.audioEle.onplay = aPlayer.startVis;
-        aPlayer.audioEle.src = aPlayer.fileList[4];
+        aPlayer.currentlyPlaying = 0;
+        aPlayer.audioEle.src = aPlayer.fileList[aPlayer.currentlyPlaying];
+        updateSongList();
+        aPlayer.audioDIV.list.innerHTML = aPlayer.fileString;
         aPlayer.cDiv.appendChild(aPlayer.audioDIV);
         return null;
+    }
+    
+    function updateSongList(){
+        aPlayer.fileString = "";
+        for(var i = 0; i < aPlayer.fileList.length; i++){
+            if(i === aPlayer.currentlyPlaying){
+                aPlayer.fileString += "(currently playing)";
+            }
+            aPlayer.fileString += (aPlayer.fileList[i])+"<br/>";
+        }
+        aPlayer.audioDIV.list.innerHTML = aPlayer.fileString;
+        LOG(aPlayer.fileString);
+    }
+    
+    function setButtonFn(){
+        aPlayer.audioDIV.nextButton.onclick = function(){    
+            aPlayer.currentlyPlaying++;
+            if(aPlayer.currentlyPlaying === aPlayer.fileList.length){
+                aPlayer.currentlyPlaying = 0;
+            }
+            updateSongList();
+            aPlayer.audioEle.src = aPlayer.fileList[aPlayer.currentlyPlaying];
+            aPlayer.audioDIV.list.innerHTML = aPlayer.fileString;
+        
+        };
+        aPlayer.audioDIV.prevButton.onclick = function(){
+            aPlayer.currentlyPlaying--;
+            if(aPlayer.currentlyPlaying === -1){
+                aPlayer.currentlyPlaying = aPlayer.fileList.length-1;
+            }
+            updateSongList();
+            aPlayer.audioEle.src = aPlayer.fileList[(aPlayer.currentlyPlaying)];
+            aPlayer.audioDIV.list.innerHTML = aPlayer.fileString;
+
+        };
     }
     
     function createAudioContext(){
@@ -58,7 +114,7 @@ function audioFW(params){
         aCTX.source = aCTX.context.createMediaElementSource(aPlayer.audioEle);
         aCTX.source.connect(aCTX.analyser);
         aCTX.analyser.connect(aCTX.context.destination);
-        aCTX.analyser.fftSize = 128;
+        aCTX.analyser.fftSize = 512;
         aPlayer.CTX = aCTX;
         return null;
     }
@@ -96,16 +152,17 @@ function audioFW(params){
         return null;
     };
     
+    
     aPlayer.lineVisual = function(timestamp){
         aPlayer.animator.ctx.clearRect(0,0, aPlayer.cWidth, aPlayer.cHeight);
         aPlayer.CTX.analyser.getByteFrequencyData(aPlayer.animator.freqData);
         aPlayer.CTX.analyser.getFloatFrequencyData(aPlayer.animator.floatFreqData);
-//        LOG(aPlayer.animator.floatFreqData[0]);
         aPlayer.animator.ctx.beginPath();
         aPlayer.animator.ctx.moveTo(0, aPlayer.cHeight/2);
         for(var i = 0; i < aPlayer.CTX.analyser.fftSize/2; i++){
-            changeColor('rgb(0,'+((i*aPlayer.animator.rectWidth)/aPlayer.cWidth)
-                    *255 + ',128)');
+            aPlayer.animator.ctx.fillStyle = aPlayer.colorFn({
+            time:(document.getElementById(aPlayer.audioEle.id).currentTime),
+            duration:document.getElementById(aPlayer.audioEle.id).duration});
             aPlayer.animator.ctx.lineTo(i*aPlayer.animator.rectWidth,
                            (aPlayer.cHeight/2) - ((aPlayer.cHeight/2) *
                                    (aPlayer.animator.freqData[i]/255)));
@@ -126,10 +183,15 @@ function audioFW(params){
         aPlayer.animator.ctx.arc(aPlayer.cWidth/2, aPlayer.cHeight/2, 50, 
                                  0, 2*Math.PI);
         var radianSlice = (2*Math.PI)/aPlayer.animator.freqData.length;
-        aPlayer.animator.ctx.strokeStyle = "red";
+        aPlayer.animator.ctx.strokeStyle = params.colorFn({
+            time:(document.getElementById(aPlayer.audioEle.id).currentTime),
+            duration:document.getElementById(aPlayer.audioEle.id).duration});
         aPlayer.animator.ctx.lineWidth = 2;
         aPlayer.animator.ctx.stroke();
-        aPlayer.animator.ctx.fillStyle = "red";
+        aPlayer.animator.ctx.fillStyle = aPlayer.colorFn({
+            time:(document.getElementById(aPlayer.audioEle.id).currentTime),
+            duration:document.getElementById(aPlayer.audioEle.id).duration});
+        var lineLength = aPlayer.cWidth/2;
         for(var i = 0; i < aPlayer.CTX.analyser.fftSize/2; i++){
             var location = radianSlice * i;
             var startX = 50 * (Math.cos(location));
@@ -138,39 +200,14 @@ function audioFW(params){
             (aPlayer.cHeight/2)-startY);
             aPlayer.animator.ctx.rotate((location - (Math.PI/2))/-1);
             aPlayer.animator.ctx.fillRect(0,
-            0, 1, -1*aPlayer.animator.freqData[i]);
-//            aPlayer.animator.ctx.fillRect((startX + (aPlayer.cWidth/2))/-2,
-//            ((aPlayer.cHeight/2)-startY)/-2, 1, aPlayer.animator.freqData[i]);
+            0, 1, (-1*aPlayer.animator.freqData[i]/255) * lineLength);
             aPlayer.animator.ctx.rotate(0);
         }
     };
     
-    aPlayer.gridVisual = function(timestamp){
-        aPlayer.CTX.analyser.getByteFrequencyData(aPlayer.animator.freqData);
-        var rectWidth = aPlayer.cWidth / aPlayer.grid.length;
-        var rectHeight = aPlayer.cHeight / aPlayer.grid[0].length;
-//        aPlayer.animator.ctx.clearRect(0,0, aPlayer.cWidth, aPlayer.cHeight);
-        for(var i = 0; i < aPlayer.CTX.analyser.fftSize/2; i++){
-            var gridX = (i * (Math.floor(Math.random()*20))) % aPlayer.grid.length;
-            var gridY = aPlayer.animator.freqData[i] * Math.floor(Math.random() * 20)
-                        % aPlayer.grid[0].length;
-            changeColor('rgb(0,'+ aPlayer.animator.freqData[i]
-                     + ',128)');
-            grid[gridX][gridY].color = 'rgb(0,'+ aPlayer.animator.freqData[i]+ ',128)';
-            aPlayer.animator.ctx.fillRect(aPlayer.grid[gridX][gridY].x,
-                                          aPlayer.grid[gridX][gridY].y,
-                                          rectWidth, rectHeight);
-        }
-        
-        for(var i = 0; i < aPlayer.grid.length; i++){
-            for(var j = 0; j < aPlayer.grid[0].length; j++){
-                
-            }
-        }
+    aPlayer.setVolume = function(nVolume){
+        aPlayer.audioEle.volume = nVolume;
     };
-    
-    
-    
     
     function changeColor(colorVal){
         aPlayer.animator.ctx.fillStyle = colorVal;
@@ -178,69 +215,59 @@ function audioFW(params){
     }
     
     aPlayer.startVis = function(){
-        aPlayer.requestVar = window.requestAnimationFrame(aPlayer.visualFn);
+        if(aPlayer.visual){
+            aPlayer.requestVar = window.requestAnimationFrame(aPlayer.visualFn);
+        }
         aPlayer.isPlaying = true;
     };
     
     aPlayer.stopVis = function(){
-        window.cancelAnimationFrame(aPlayer.requestVar);
+        if(aPlayer.visual){
+            window.cancelAnimationFrame(aPlayer.requestVar);
+ 
+        }
         aPlayer.isPlaying = false;
     };
     
-    if(params.visualStyle === "bar"){
-        aPlayer.visualFn = aPlayer.barVisual;
-    }
-    else if(params.visualStyle === "circle"){
-        aPlayer.visualFn = aPlayer.circleVisual;
-    }
-    else if(params.visualStyle === "line"){
-        aPlayer.visualFn = aPlayer.lineVisual;
-    }
-    else if(params.visualStyle === "grid"){
-        aPlayer.visualFn = aPlayer.gridVisual;
-        aPlayer.grid = makeGrid({x:aPlayer.cWidth, y:aPlayer.cHeight});
-    }
+    aPlayer.changeVisual = function(nVisual){
+        if(nVisual === "bar"){
+            aPlayer.visualFn = aPlayer.barVisual;
+        }
+        else if(nVisual === "circle"){
+            aPlayer.cHeight = aPlayer.cWidth;
+            aPlayer.animator.canvasEle.height = aPlayer.cHeight;
+            aPlayer.visualFn = aPlayer.circleVisual;
+        }
+        else if(nVisual === "line"){
+            aPlayer.visualFn = aPlayer.lineVisual;
+        }
+        else{
+            alert("Invalid visual type entered, setting to 'bar'.");
+            aPlayer.visualFn = aPlayer.barVisual;
+        }
+    };
     
     createPlayer();
     createAudioContext();
     if(params.visualize){
+        aPlayer.visual = true;
         createVisualizer();
+        aPlayer.changeVisual(params.visualStyle);
+
         setInterval(function(){
             aPlayer.requestVar = window.requestAnimationFrame(aPlayer.visualFn);
         },25);
+        window.addEventListener("resize", function(){
+            aPlayer.animator.rectWidth = Math.ceil(aPlayer.cDiv.clientWidth / 
+                    (aPlayer.CTX.analyser.fftSize/2));
+            aPlayer.animator.canvasEle.width = aPlayer.cDiv.clientWidth;
+            aPlayer.cWidth = aPlayer.cDiv.clientWidth;
+        });
     }
     
-    document.body.onresize = function(){
-        aPlayer.animator.rectWidth = Math.ceil(aPlayer.cDiv.clientWidth / 
-                (aPlayer.CTX.analyser.fftSize/2));
-        aPlayer.animator.canvasEle.width = aPlayer.cDiv.clientWidth;
-        aPlayer.cWidth = aPlayer.cDiv.clientWidth;
-    };
+
     LOG('Done');
     return aPlayer;
 }
 
-function ripples(params){
-    var rippleList = {};
-    rippleList.x = params.x;
-    rippleList.y = params.y;
-    rippleList.growthRate = params.growthRate;
-    rippleList.radius = 2;
-    rippleList.grow = function(){
-        if(rippleList.radius < 100){
-            rippleList.radius += rippleList.growthRate;
-        }
-    };
-    return rippleList;
-}
 
-function makeGrid(params){
-    var grid = [];
-    for(var i = 0; i < params.x; i+=10){
-        grid.push([]);
-        for(var j = 0; j < params.y; j+=10){
-            grid[grid.length - 1].push({x:i,y:j});
-        }
-    }
-    return grid;
-}
